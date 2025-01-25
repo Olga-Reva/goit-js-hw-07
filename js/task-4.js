16cdb70489fbb5350628162b2f3925f6fe720acb
Reva Olga <Reva.Olga.Vladimirovna@gmail.com>
2025-01-25 02:28:07 +0200

    task number four

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  const formData = {
+    [form.elements.email.name]: form.elements.email.value.trim(),
+    [form.elements.password.name]: form.elements.password.value.trim(),
+  };
+
+  if (!formData.email || !formData.password) {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  console.log(formData);
+  form.reset();
+});
